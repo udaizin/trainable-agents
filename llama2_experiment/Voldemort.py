8ff4c9894931dcfa806cb2eb291824a9565173e9
udaizin <iwanttostudypython21@gmail.com>
2024-06-24 02:52:55 +0000

    rename self_experiment を original_model_experimentに変更

--- llama2_experiment/Voldemort.py
+++ llama2_experiment/Voldemort.py
@@ -1,13 +1,16 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers.trainer_utils import get_last_checkpoint
+from peft import PeftModel, PeftConfig
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 # set up tokenizer and model
-checkpoint = "/work/trainable-agents/FastChat/models/character-llm-voldemort-7b-wdiff"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
+checkpoint_dir = '/work/trainable-agents/models/characte-llm-Voldemort-llama2-7B-wdiff/'
+last_checkpoint = get_last_checkpoint(checkpoint_dir)
+tokenizer = AutoTokenizer.from_pretrained(last_checkpoint)
+model = AutoModelForCausalLM.from_pretrained(last_checkpoint).to(device)
 
 '''
 Generate a response from Beethoven model
